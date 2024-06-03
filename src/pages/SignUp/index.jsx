d36ac891ac5b51bbaa4bff2eb4c91aec4931eb52
Vitor Martini <vitor-martini@outlook.com>
2024-06-02 22:07:37 -0300

    feat: connecting the sighUp interface with the api

--- src/pages/SignUp/index.jsx
+++ src/pages/SignUp/index.jsx
@@ -2,9 +2,35 @@ import { Container, Form } from "./styles"
 import { FiMail, FiLock, FiUser } from "react-icons/fi"
 import { Input } from "../../components/Input"
 import { Button } from "../../components/Button"
-import { Link } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
+import { useState } from "react"
+import { api } from "../../services/api"
 
 export function SignUp() {
+  const [name, setName] = useState("")
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+  const navigate = useNavigate();
+
+  function hangleSignUp() {
+    if (!name || !email || !password){
+      return alert("Preencha todos os campos!")
+    }
+
+    api.post("/users", { name, email, password })
+      .then(() => {
+        alert("Usuários cadastrado com sucesso!")
+        navigate("/")
+      })
+      .catch(error => {
+        if(error.response) {
+          alert(error.response.data.message)
+        } else {
+          alert("Não foi possível cadastrar.")
+        }
+      })
+  }
+
   return (
     <Container>
       <Form>
@@ -15,22 +41,28 @@ export function SignUp() {
         <Input
           placeholder="Nome"
           type="text"
-          icon={FiUser}>
-        </Input>
-
+          icon={FiUser}
+          onChange={e => setName(e.target.value)}
+        />
+        
         <Input
           placeholder="E-mail"
           type="text"
           icon={FiMail}
-        ></Input>
+          onChange={e => setEmail(e.target.value)}
+        />
 
         <Input
           placeholder="Senha"
           type="password"
-          icon={FiLock}>
-        </Input>
+          icon={FiLock}
+          onChange={e => setPassword(e.target.value)}
+        />
 
-        <Button title="Cadastrar"/>
+        <Button 
+          title="Cadastrar"
+          onClick={hangleSignUp}
+        />
 
         <Link to="/">Voltar</Link>
       </Form>
