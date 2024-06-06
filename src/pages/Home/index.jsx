ec601fd616a8073265af79148e256ace62a16027
Vitor Martini <vitor-martini@outlook.com>
2024-06-05 22:26:07 -0300

    feat: tags filter

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -5,8 +5,34 @@ import { Input } from '../../components/Input';
 import { Section } from '../../components/Section';
 import { Note } from '../../components/Note';
 import { FiPlus, FiSearch } from 'react-icons/fi';
+import { useEffect, useState } from 'react';
+import { api } from '../../services/api';
 
 export function Home() {
+
+  const [tags, setTags] = useState([])
+  const [selectedTags, setSelectedTags] = useState([])
+
+  function handleSelectedTag(tagName) {
+    const isAlreadySelected = selectedTags.includes(tagName)
+
+    if (isAlreadySelected) {
+      setSelectedTags(selectedTags.filter(tag => tag !== tagName))
+      return 
+    }
+
+    setSelectedTags(prevState => [...prevState, tagName])
+  }
+
+  useEffect(() => {
+    async function fetchTags(){
+      const response = await api.get("/tags")
+      setTags(response.data)
+    }
+
+    fetchTags()
+  }, []) 
+
   return (
     <Container>
       <Brand>
@@ -14,10 +40,23 @@ export function Home() {
       </Brand>
       <Header />
       <Menu>
-        <li><TextButton title="Todos" isActive /></li>
-        <li><TextButton title="AAA" /></li>
-        <li><TextButton title="BBB" /></li>
-        <li><TextButton title="CCC" /></li>
+        <li>
+          <TextButton 
+            title="Todos" 
+            isActive={selectedTags.length === 0} 
+            onClick={() => setSelectedTags([])}
+          /></li>
+        {
+          tags && tags.map((tag, index) => (
+            <li key={index}>
+              <TextButton 
+                title={tag.name} 
+                isActive={selectedTags.includes(tag.name)}
+                onClick={() => handleSelectedTag(tag.name)}
+              />
+            </li>
+          ))
+        }
       </Menu>
       
       <Search>
