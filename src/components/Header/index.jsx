7fe982a891041373035be1ded77dee3ed877361c
Vitor Martini <vitor-martini@outlook.com>
2024-06-05 21:24:01 -0300

    fix: redirect the user when log out

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,12 +1,19 @@
 import { RiShutDownLine } from "react-icons/ri";
 import { Container, Profile, Logout } from "./styles";
 import { useAuth } from "../../hooks/auth";
+import { useNavigate } from "react-router-dom";
 import { api } from "../../services/api";
 import avatarPlaceholder from "../../assets/avatar_placeholder.svg"
 
 export function Header() {
+  const navigate = useNavigate();
   const { signOut, user } = useAuth();
   const avatarUrl = user.avatar ? `${api.defaults.baseURL}/files/${user.avatar}` : avatarPlaceholder
+
+  function handleSignOut() {
+    signOut()
+    navigate("/")
+  }
 
   return (
     <Container>
@@ -19,7 +26,7 @@ export function Header() {
         </div>  
       </Profile>
 
-      <Logout onClick={signOut}>
+      <Logout onClick={handleSignOut}>
         <RiShutDownLine/>
       </Logout>
     </Container>
